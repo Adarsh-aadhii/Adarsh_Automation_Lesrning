f2522ae4035308ca41b9fc206fced91bb19e427b
Adarsh-aadhii <adarshks999@gmail.com>
2024-10-09 16:06:38 +0530

    all the files

---
+++ src/Oct/ex_03102024/Task3.java
@@ -0,0 +1,13 @@
+package Oct.ex_03102024;
+
+import java.sql.SQLOutput;
+
+public class Task3
+{
+    public static void main(String[] args) {
+      System.out.println(5+5);
+        System.out.println(5-5);
+        System.out.println(5*5);
+        System.out.println(50/10);
+    }
+}
